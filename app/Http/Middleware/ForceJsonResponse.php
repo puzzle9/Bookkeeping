f09ec56b022fbf0528096c7d92dab6f8bc0ca479
puzzle <happypuzzle@126.com>
2022-07-29 13:53:11 +0800

    add api response default json

---
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+// https://stackoverflow.com/a/58043508/6686061
+class ForceJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request                                                                          $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        return $next($request);
+    }
+}
