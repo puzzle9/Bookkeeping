87f069f272260ab2c2bcfb82ec0593138b9d8cf6
puzzle <happypuzzle@126.com>
2022-09-11 14:03:24 +0800

    perf: pwa

--- resources/assets/wap/main.ts
+++ resources/assets/wap/main.ts
@@ -3,6 +3,17 @@ import App from '@wap/App.vue'
 
 const app = createApp(App)
 
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+    onNeedRefresh() {},
+    onOfflineReady() {},
+    onRegisterError(error) {
+        console.log(`sw register error:`, error)
+    },
+})
+
 import router from '@wap/router'
 import store from '@wap/store'
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,32 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
+        VitePWA({
+            injectRegister: 'inline',
+            manifest: {
+                lang: 'zh-CN',
+                name: 'bookkeeping',
+                short_name: '记账',
+                description: '一款复式记账的记账',
+                start_url: '/',
+                scope: '/',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: '/favicon/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/favicon/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
     ],
     resolve: {
         alias: {
